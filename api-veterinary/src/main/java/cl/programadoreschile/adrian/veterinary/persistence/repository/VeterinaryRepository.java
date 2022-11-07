3cc49d3d1de9591c8c9b2ce1ccdb3a2d66984749
xh31806 <adrian.soto.contractor@forum.cl>
2022-11-06 23:48:20 -0300

    fix: internacionalización

--- api-veterinary/src/main/java/cl/programadoreschile/adrian/veterinary/persistence/repository/VeterinaryRepository.java
+++ api-veterinary/src/main/java/cl/programadoreschile/adrian/veterinary/persistence/repository/VeterinaryRepository.java
@@ -5,6 +5,8 @@ import cl.programadoreschile.adrian.veterinary.error.APIException;
 import cl.programadoreschile.adrian.veterinary.persistence.models.VeterinaryDAO;
 import cl.programadoreschile.adrian.veterinary.persistence.repository.mongo.VeterinaryMongoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
@@ -13,27 +15,33 @@ import org.springframework.stereotype.Repository;
 public class VeterinaryRepository implements VeterinaryGateway {
 
     private final VeterinaryMongoRepository veterinaryMongoRepository;
+    private final MessageSource messages;
+
     @Override
     public VeterinaryDAO createVeterinary(VeterinaryDAO newVeterinary) {
         VeterinaryDAO veterinary = findByProfessionalLicenseNumber(newVeterinary.getProfessionalLicenseNumber());
-        if(veterinary != null){
-            throw new APIException("El veterinario ya existe", HttpStatus.CONFLICT);
-        }else{
+        if (veterinary != null) {
+            final String rspFormatMsg = messages.getMessage("veterinary.duplicated.message", null, LocaleContextHolder.getLocale());
+            final String responseMessage = String.format(rspFormatMsg, newVeterinary.getProfessionalLicenseNumber());
+            throw new APIException(responseMessage, HttpStatus.CONFLICT);
+        } else {
             return veterinaryMongoRepository.save(newVeterinary);
         }
     }
 
     @Override
-    public VeterinaryDAO findByProfessionalLicenseNumber(String professionalLicenseNumber){
+    public VeterinaryDAO findByProfessionalLicenseNumber(String professionalLicenseNumber) {
         return veterinaryMongoRepository.findByProfessionalLicenseNumberEqualsIgnoreCase(professionalLicenseNumber);
     }
 
     @Override
-    public void delete(String professionalLicenseNumber){
+    public void delete(String professionalLicenseNumber) {
         VeterinaryDAO veterinary = findByProfessionalLicenseNumber(professionalLicenseNumber);
-        if(veterinary == null){
-            throw new APIException("El veterinario no existe", HttpStatus.CONFLICT);
-        }else{
+        if (veterinary == null) {
+            final String rspFormatMsg = messages.getMessage("veterinary.not.exist.message", null, LocaleContextHolder.getLocale());
+            final String responseMessage = String.format(rspFormatMsg, professionalLicenseNumber);
+            throw new APIException(responseMessage, HttpStatus.CONFLICT);
+        } else {
             veterinaryMongoRepository
                     .deleteVeterinaryDAOByProfessionalLicenseNumberEqualsIgnoreCase(professionalLicenseNumber);
         }
